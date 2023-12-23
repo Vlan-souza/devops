5686aba28b2a8d6c3a6180ec2cb17b7b1f77a003
Vagrant User <vagrant@controle>
2023-12-23 16:30:46 -0300

    inicial

---
+++ script2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+cd /tmp
+wget https://apt.puppet.com/puppet7-release-bullseye.deb
+dpkg -i puppet7-release-bullseye.deb
+apt-get update
+apt-get install vim puppet-agent -y
+
+
+
+#vanderlan peixoto de souza
